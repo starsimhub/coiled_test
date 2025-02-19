d5183216cdbec6a82be1203a77652821f3780845
Paula Sanz-Leon <pmsl.academic@gmail.com>
2025-02-20 07:35:31 +1000

    Include fixes from @jrbourbeau; cluster as a cm; keep db

--- coiled_calib.py
+++ coiled_calib.py
@@ -1,15 +1,15 @@
-from dask.distributed import get_client, wait
+from dask.distributed import wait
 import starsim as ss
 import sciris as sc
 import pandas as pd
 import numpy as np
 import coiled
-import optuna as op
 
-debug = False # If true, will run in serial
+debug = False  # If true, will run in serial
 total_trials = [100, 10][debug]
 n_agents = 2_000
 do_plot = True
+
 
 class CoiledCalibration(ss.Calibration):
 
@@ -24,74 +24,75 @@ class CoiledCalibration(ss.Calibration):
             verbose (bool): whether to print output from each trial
             kwargs (dict): if supplied, overwrite stored run_args (n_trials, n_workers, etc.)
         """
+        import optuna as op
+
         # Load and validate calibration parameters
         if calib_pars is not None:
             self.calib_pars = calib_pars
-        self.run_args.update(kwargs) # Update optuna settings
+        self.run_args.update(kwargs)  # Update optuna settings
 
-        # Run the optimization
-        t0 = sc.tic()
-        self.study = self.make_study()
+        with coiled.Cluster(
+                n_workers=10,
+                name='StarsimCalibrationOnCoiled',
+        ) as cluster:
+            with cluster.get_client() as client:
+                # Run the optimization
+                t0 = sc.tic()
+                self.run_args.storage = op.integration.DaskStorage(storage=None,
+                                                                   client=client)
+                self.study = self.make_study()
 
-        #self.run_workers() # <-- This is the line from the base class to run locally using multiprocessing
-        ################################
-        cluster = coiled.Cluster(
-            n_workers=10,
-            name='StarsimCalibrationOnCoiled'
-        )
+                # self.run_workers() # <-- This is the line from the base class to run locally using multiprocessing
+                futures = [
+                    client.submit(
+                        self.study.optimize,
+                        self.run_trial,
+                        n_trials=self.run_args.n_trials,
+                        pure=False
+                    )
+                    for _ in range(self.run_args.n_workers)
+                ]
 
-        # backend_storage = op.storages.InMemoryStorage() --> not sure this is needed
-        dask_storage = op.integration.DaskStorage(storage=None,
-                                                  client=cluster.get_client())
+                wait(futures)
+                #################################
 
-        client = cluster.get_client()
-        futures = [
-            client.submit(
-                self.study.optimize,
-                self.run_trial,
-                n_trials=self.run_args.n_trials,
-                pure=False
-            )
-            for _ in range(self.run_args.n_workers)
-        ]
+                study = op.load_study(storage=self.run_args.storage,
+                                      study_name=self.run_args.study_name,
+                                      sampler=self.run_args.sampler)
+                self.best_pars = sc.objdict(study.best_params)
+                self.elapsed = sc.toc(t0, output=True)
 
-        wait(futures)
-        #################################
-
-        study = op.load_study(storage=dask_storage, study_name=self.run_args.study_name, sampler=self.run_args.sampler)
-        self.best_pars = sc.objdict(study.best_params)
-        self.elapsed = sc.toc(t0, output=True)
-
-        self.sim_results = []
-        if load:
-            if self.verbose: print('Loading saved results...')
-            for trial in study.trials:
-                n = trial.number
-                try:
-                    filename = self.tmp_filename % trial.number
-                    results = sc.load(filename)
-                    self.sim_results.append(results)
-                    if tidyup:
+                self.sim_results = []
+                if load:
+                    if self.verbose: print('Loading saved results...')
+                    for trial in study.trials:
+                        n = trial.number
                         try:
-                            os.remove(filename)
-                            if self.verbose: print(f'    Removed temporary file {filename}')
+                            filename = self.tmp_filename % trial.number
+                            results = sc.load(filename)
+                            self.sim_results.append(results)
+                            if tidyup:
+                                try:
+                                    os.remove(filename)
+                                    if self.verbose: print(
+                                        f'    Removed temporary file {filename}')
+                                except Exception as E:
+                                    errormsg = f'Could not remove {filename}: {str(E)}'
+                                    if self.verbose: print(errormsg)
+                            if self.verbose: print(f'  Loaded trial {n}')
                         except Exception as E:
-                            errormsg = f'Could not remove {filename}: {str(E)}'
+                            errormsg = f'Warning, could not load trial {n}: {str(E)}'
                             if self.verbose: print(errormsg)
-                    if self.verbose: print(f'  Loaded trial {n}')
-                except Exception as E:
-                    errormsg = f'Warning, could not load trial {n}: {str(E)}'
-                    if self.verbose: print(errormsg)
 
-        # Compare the results
-        self.parse_study(study)
+                # Compare the results
+                self.parse_study(study)
 
-        if self.verbose: print('Best pars:', self.best_pars)
+                if self.verbose: print('Best pars:', self.best_pars)
 
-        # Tidy up
-        self.calibrated = True
-        if not self.run_args.keep_db:
-            self.remove_db()
+                # Tidy up
+                self.calibrated = True
+                if not self.run_args.keep_db:
+                    self.remove_db()
 
         return self
 
@@ -102,20 +103,20 @@ class CoiledCalibration(ss.Calibration):
 
 def make_sim():
     sir = ss.SIR(
-        beta = ss.beta(0.075),
-        init_prev = ss.bernoulli(0.02),
+        beta=ss.beta(0.075),
+        init_prev=ss.bernoulli(0.02),
     )
     random = ss.RandomNet(n_contacts=ss.poisson(4))
 
     sim = ss.Sim(
-        n_agents = n_agents,
-        start = sc.date('2020-01-01'),
-        stop = sc.date('2020-02-12'),
-        dt = 1,
-        unit = 'day',
-        diseases = sir,
-        networks = random,
-        verbose = 0,
+        n_agents=n_agents,
+        start=sc.date('2020-01-01'),
+        stop=sc.date('2020-02-12'),
+        dt=1,
+        unit='day',
+        diseases=sir,
+        networks=random,
+        verbose=0,
     )
 
     return sim
@@ -126,8 +127,8 @@ def build_sim(sim, calib_pars, **kwargs):
 
     reps = kwargs.get('n_reps', 1)
 
-    sir = sim.pars.diseases # There is only one disease in this simulation and it is a SIR
-    net = sim.pars.networks # There is only one network in this simulation and it is a RandomNet
+    sir = sim.pars.diseases  # There is only one disease in this simulation and it is a SIR
+    net = sim.pars.networks  # There is only one network in this simulation and it is a RandomNet
 
     for k, pars in calib_pars.items():
         if k == 'rand_seed':
@@ -148,68 +149,75 @@ def build_sim(sim, calib_pars, **kwargs):
         return sim
 
     # Ignoring the random seed if provided via the reseed=True option in Calibration
-    ms = ss.MultiSim(sim, iterpars=dict(rand_seed=np.random.randint(0, 1e6, reps)), initialize=True, debug=True, parallel=False)
+    ms = ss.MultiSim(sim,
+                     iterpars=dict(rand_seed=np.random.randint(0, 1e6, reps)),
+                     initialize=True, debug=True, parallel=False)
     return ms
 
 
 def extract_prevalence(sim):
     df = pd.DataFrame({
-            'x': sim.results.sir.n_infected, # Instead of prevalence, let's compute it from infected and n_alive
-            'n': sim.results.n_alive,
-        }, index=pd.Index(sim.results.timevec, name='t'))
+        'x': sim.results.sir.n_infected,
+        # Instead of prevalence, let's compute it from infected and n_alive
+        'n': sim.results.n_alive,
+    }, index=pd.Index(sim.results.timevec, name='t'))
     return df
 
+
 def test_coiled(do_plot=True):
-    sc.heading('Testing a single parameter (beta) with a normally distributed likelihood, one data point')
+    sc.heading(
+        'Testing a single parameter (beta) with a normally distributed likelihood, one data point')
 
     # Define the calibration parameters
     calib_pars = dict(
-        beta = dict(low=0.01, high=0.30, guess=0.15, suggest_type='suggest_float', log=True),
+        beta=dict(low=0.01, high=0.30, guess=0.15, suggest_type='suggest_float',
+                  log=True),
     )
 
     # Make the sim and data
     sim = make_sim()
 
     prevalence = ss.Normal(
-        name = 'Disease prevalence',
-        conform = 'prevalent',
+        name='Disease prevalence',
+        conform='prevalent',
 
-        expected = pd.DataFrame({
-            'x': [0.13],    # Prevalence of infection
-        }, index=pd.Index([ss.date('2020-01-12')], name='t')), # On these dates
+        expected=pd.DataFrame({
+            'x': [0.13],  # Prevalence of infection
+        }, index=pd.Index([ss.date('2020-01-12')], name='t')),  # On these dates
 
-        #expected = pd.DataFrame({
+        # expected = pd.DataFrame({
         #    'x': [0.13, 0.16, 0.06],    # Prevalence of infection
-        #}, index=pd.Index([ss.date(d) for d in ['2020-01-12', '2020-01-25', '2020-02-02']], name='t')), # On these dates
+        # }, index=pd.Index([ss.date(d) for d in ['2020-01-12', '2020-01-25', '2020-02-02']], name='t')), # On these dates
 
-        #expected = pd.DataFrame({
+        # expected = pd.DataFrame({
         #    'x':  [740], # Number of new infections
         #    'n':  [100],   # Number of person-years
         #    't':  [ss.date(d) for d in ['2020-01-07']], # Between t and t1
         #    't1': [ss.date(d) for d in ['2020-01-08']],
-        #}).set_index(['t', 't1']),
-        
-        extract_fn = extract_prevalence,
-        #extract_fn = lambda sim: pd.DataFrame({
+        # }).set_index(['t', 't1']),
+
+        extract_fn=extract_prevalence,
+        # extract_fn = lambda sim: pd.DataFrame({
         #    'x': sim.results.sir.n_infected, # Instead of prevalence, let's compute it from infected and n_alive
         #    'n': sim.results.n_alive,
-        #}, index=pd.Index(sim.results.timevec, name='t')),
+        # }, index=pd.Index(sim.results.timevec, name='t')),
 
-        sigma2 = 0.05, # (num_replicates/sigma2_model + 1/sigma2_data)^-1
+        sigma2=0.05,  # (num_replicates/sigma2_model + 1/sigma2_data)^-1
     )
 
     # Make the calibration
     calib = CoiledCalibration(
-        calib_pars = calib_pars,
-        sim = sim,
-        build_fn = build_sim,
-        build_kw = dict(n_reps=5), # Reps per point
-        reseed = False,
-        components = [prevalence],
-        total_trials = total_trials,
-        n_workers = None, # None indicates to use all available CPUs
-        die = True,
-        debug = debug,
+        calib_pars=calib_pars,
+        sim=sim,
+        build_fn=build_sim,
+        build_kw=dict(n_reps=5),  # Reps per point
+        reseed=False,
+        components=[prevalence],
+        total_trials=total_trials,
+        n_workers=None,  # None indicates to use all available CPUs
+        die=True,
+        debug=debug,
+        keep_db=True,
     )
 
     # Perform the calibration
@@ -224,9 +232,11 @@ def test_coiled(do_plot=True):
         calib.plot_final()
         calib.plot(bootstrap=False)
         calib.plot(bootstrap=True)
-        calib.plot_optuna(['plot_param_importances', 'plot_optimization_history'])
+        calib.plot_optuna(
+            ['plot_param_importances', 'plot_optimization_history'])
 
     return sim, calib
+
 
 if __name__ == '__main__':
     test_coiled()
